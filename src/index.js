42de055b2ae3edd43ca36d5ffe2b45b5f206e597
unknown <ronaldobarros1008@gmail.com>
2021-03-13 02:38:00 -0300

    commit de teste git

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Calculator from './main/Calculator';
 import registerServiceWorker from './registerServiceWorker';
 
+//teste git
 ReactDOM.render(
     <div>
         <h1>Calculadora</h1>
